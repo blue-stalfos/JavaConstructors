95f04082a4a9203c3729df5f66397be1a274ff71
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-01-28 12:04:26 -0500

    first commit

---
+++ src/com/richardbatista/Main.java
@@ -0,0 +1,16 @@
+package com.richardbatista;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Account bobsAccount = new Account("12345", 0.00, "Bob Brown",
+			    "myemail@bob.com", "123-456-7890");
+
+	    bobsAccount.withdrawal(100.0);
+	    bobsAccount.deposit(50.0);
+	    bobsAccount.withdrawal(100.0);
+	    bobsAccount.deposit(51.0);
+	    bobsAccount.withdrawal(100.0);
+
+    }
+}
